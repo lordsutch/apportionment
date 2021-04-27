7fae7ca23fc304ee6d40f891b52aaa4469342df2
Chris Lawrence <lordsutch@gmail.com>
2021-04-26 21:46:26 -0400

    Refactor to allow --no-losers with both apportionment methods.

--- apportionment.py
+++ apportionment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Apportionment by equal proportions method
+# Apportionment calculations using various methods
 #
 # Copyright (c) 2021 Christopher N. Lawrence <lordsutch@gmail.com>
 
@@ -30,47 +30,49 @@ import pandas as pd
 import sys
 
 
-def equal_proportions(data: pd.DataFrame, seats: int) -> pd.DataFrame:
+def equal_proportions(data: pd.DataFrame, seats: int,
+                      no_losers=False) -> pd.DataFrame:
     total_population = data['POPULATION'].sum()
     
     # Each state gets one seat to start
     data["SEATS"] = 1
 
-    while (seatcount := data.SEATS.sum()) < seats:
+    while ((seatcount := data.SEATS.sum()) < seats or
+           no_losers and (data.SEATS < data.APP2010).any()):
         data['PRIORITY'] = data.POPULATION / np.sqrt(data.SEATS*(data.SEATS+1))
         state_maxpri = data.PRIORITY.idxmax()
         maxpri = data.PRIORITY.max()
         data.loc[state_maxpri, 'SEATS'] += 1
         print(f'{seatcount+1:4d} {data.SEATS[state_maxpri]:2d} {state_maxpri:30} {maxpri:12.3f}', file=sys.stderr)
-    return pd.DataFrame(dict(population=data.POPULATION, seats=data.SEATS))
+
+    # Sanity check
+    assert no_losers or data.SEATS.sum() == seats
+
+    return pd.DataFrame(dict(POPULATION=data.POPULATION, SEATS=data.SEATS))
 
 
-def equal_proportions_no_losers(data: pd.DataFrame,
-                                seats: int) -> pd.DataFrame:
-    total_population = data['POPULATION'].sum()
+def largest_remainders(data: pd.DataFrame, seats: int,
+                       no_losers=False) -> pd.DataFrame:
+    if no_losers:
+        while 1:
+            # Solve this iteratively... otherwise I think we might run into the
+            # Alabama paradox
+            result = largest_remainders(data, seats, no_losers=False)
+            if (result.SEATS < data.APP2010).any():
+                seats += 1
+            else:
+                print(f'{seats} seats required for no losers.',
+                      file=sys.stderr)
+                return result
     
-    # Each state gets one seat to start
-    data["SEATS"] = 1
-
-    while (data.SEATS < data.APP2010).any():
-        seatcount = data.SEATS.sum()
-        data['PRIORITY'] = data.POPULATION / np.sqrt(data.SEATS*(data.SEATS+1))
-        state_maxpri = data.PRIORITY.idxmax()
-        maxpri = data.PRIORITY.max()
-        data.loc[state_maxpri, 'SEATS'] += 1
-        print(f'{seatcount+1:4d} {data.SEATS[state_maxpri]:2d} {state_maxpri:30} {maxpri:12.3f} {data.SEATS[state_maxpri]-data.APP2020[state_maxpri]:2d}', file=sys.stderr)
-    return pd.DataFrame(dict(population=data.POPULATION, seats=data.SEATS))
-
-
-def largest_remainders(data: pd.DataFrame, seats: int) -> pd.DataFrame:
     total_population = data['POPULATION'].sum()
 
     # Hare/Hamilton quota
-    quota = total_population / seats
-    
+    quota = math.floor(total_population / seats)
+
     # Each state gets the total population / seats to start
     # with a minimum of 1 seat
-    data["SEATS"] = data.POPULATION // quota
+    data["SEATS"] = data.POPULATION // quota # Use integer division
     data.loc[data.SEATS < 1, 'SEATS'] = 1
 
     remaining_seats = int(seats - data.SEATS.sum())
@@ -86,7 +88,7 @@ def largest_remainders(data: pd.DataFrame, seats: int) -> pd.DataFrame:
     # Sanity check
     assert data.SEATS.sum() == seats
 
-    return pd.DataFrame(dict(population=data.POPULATION, seats=data.SEATS.astype(int)))
+    return pd.DataFrame(dict(POPULATION=data.POPULATION, SEATS=data.SEATS.astype(int)))
 
 
 def main() -> None:
@@ -98,18 +100,21 @@ def main() -> None:
                             help='use cube root rule to decide number of seats')
     seat_group.add_argument('--wyoming-rule', '-w', action='store_true',
                             help='use "Wyoming rule" (smallest state entitled to exactly one seat) to decide number of seats')
+    parser.add_argument('--no-losers', action='store_true',
+                        help='make no states lose seats vs. 2010; seats will be treated as a minimum')
+    method_group = parser.add_mutually_exclusive_group(required=False)
+    method_group.add_argument('--equal-proportions', dest='app_method',
+                              const=equal_proportions, action='store_const',
+                              default=equal_proportions,
+                              help='use the equal proportions method (default)')
+    method_group.add_argument('--largest-remainders', dest='app_method',
+                              const=largest_remainders, action='store_const',
+                              help='use the largest remainders method')
     parser.add_argument('--output', '-o', type=str, default=None,
                         help='output file (default: stdout)')
     parser.add_argument('input', default='apportionment-2020-table01.csv',
                         metavar='INPUT', nargs='?',
                         help='input file with apportionment table', type=str)
-    parser.add_argument('--no-losers', dest='app_method',
-                        default=equal_proportions, action='store_const',
-                        const=equal_proportions_no_losers,
-                        help='ignore seats, make no states lose using equal proportions')
-    parser.add_argument('--largest-remainders', dest='app_method',
-                        const=largest_remainders, action='store_const',
-                        help='use the largest remainders method')
     
     args = parser.parse_args()
 
@@ -122,11 +127,16 @@ def main() -> None:
         seats = data.POPULATION.sum() // data.POPULATION.min()
     else:
         seats = args.seats
-    print(f'Apportioning {seats} seats...', file=sys.stderr)
-    
-    result = args.app_method(data, seats)
 
-    result['difference'] = result.seats - data.APP2020
+    if not args.no_losers:
+        print(f'Apportioning {seats} seats...', file=sys.stderr)
+    else:
+        print('Ensuring all states keep existing seats...', file=sys.stderr)
+    
+    result = args.app_method(data, seats, args.no_losers)
+
+    result['Difference2020'] = result.SEATS - data.APP2020
+    result['Difference2010'] = result.SEATS - data.APP2010
 
     result.to_csv(args.output or sys.stdout)
     
